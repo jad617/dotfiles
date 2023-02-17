44fd21e2da0df49c97c732b758a48edf1525cc68
jad617 <>
2023-02-17 11:55:20 -0500

    misc

--- nvim/lua/vars.lua
+++ nvim/lua/vars.lua
@@ -1,58 +1,58 @@
 M = {}
 
 M.lsp = {
-	bashls = {},
-	dockerls = {}, -- docker
-	jsonls = {}, -- json
-	gopls = {}, -- golang
-	golangci_lint_ls = {}, -- golangci
-	groovyls = {}, -- groovy
-	pyright = {}, -- python
-	solargraph = {}, -- ruby
-	sumneko_lua = { -- lua
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" }, -- removes warning: 'Global vim is undefined'
-				},
-			},
-		},
-	},
-	tflint = {}, -- terraform docs
-	tsserver = {}, -- javascript
-	terraformls = {}, -- terraform
+  bashls = {},
+  dockerls = {}, -- docker
+  jsonls = {}, -- json
+  gopls = {}, -- golang
+  golangci_lint_ls = {}, -- golangci
+  groovyls = {}, -- groovy
+  pyright = {}, -- python
+  solargraph = {}, -- ruby
+  sumneko_lua = { -- lua
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" }, -- removes warning: 'Global vim is undefined'
+        },
+      },
+    },
+  },
+  tflint = {}, -- terraform docs
+  tsserver = {}, -- javascript
+  terraformls = {}, -- terraform
 }
 
 M.linter = {
-	formatting = {
-		"stylua",
-		"gofumpt",
-		"goimports",
-		"goimports-reviser",
-		"markdownlint",
-		"rubocop",
-		"beautysh",
-		-- "shellharden",       -- Requires Cargo
-		"shfmt",
-		-- "terraform-fmt",     -- Not found by Mason
-		"yamlfmt",
-		"black",
-		"isort",
-		"autopep8",
-		"mypy",
-	},
-	diagnostics = {
-		"flake8",
-		"golangci-lint",
-		"jsonlint",
-		"markdownlint",
-		"pylint",
-		"rubocop",
-		"shellcheck",
-		"staticcheck",
-		"tflint",
-		"yamllint",
-	},
+  formatting = {
+    -- "stylua",
+    "gofumpt",
+    "goimports",
+    "goimports-reviser",
+    "markdownlint",
+    "rubocop",
+    -- "beautysh",
+    -- "shellharden",       -- Requires Cargo
+    "shfmt",
+    -- "terraform-fmt",     -- Not found by Mason
+    "yamlfmt",
+    "black",
+    "isort",
+    "autopep8",
+    "mypy",
+  },
+  diagnostics = {
+    "flake8",
+    "golangci-lint",
+    "jsonlint",
+    "markdownlint",
+    "pylint",
+    "rubocop",
+    "shellcheck",
+    "staticcheck",
+    "tflint",
+    "yamllint",
+  },
 }
 
 return M
