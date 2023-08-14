b99b5df6405da15f85a20d65c8635d536bb42002
jad617 <elasmar.jad@gmail.com>
2023-08-13 20:02:03 -0400

    misc

---
+++ lazyvim/init.lua
@@ -0,0 +1,8 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- custom plugins config
+require("config.plugins.comfortable-motion")
+require("config.plugins.nvim-tree")
+require("config.plugins.statusline")
+require("config.plugins.markdown-preview")
