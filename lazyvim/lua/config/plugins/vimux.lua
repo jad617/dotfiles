c7814921ea4446bcea586222212f87fea81abe41
jad617 <elasmar.jad@gmail.com>
2023-09-29 23:09:53 -0400

    misc

---
+++ lazyvim/lua/config/plugins/vimux.lua
@@ -0,0 +1,16 @@
+-- [[ local vars ]]
+local map = vim.api.nvim_set_keymap -- set keys
+local options = { noremap = true, silent = true }
+
+------------------------------------------------------------
+-- [[ Key Bindings ]]
+------------------------------------------------------------
+-- [[ Linux ]]
+map("n", "<A-8>", ":VimuxRunCommand('python3 ' . bufname('%'))<CR>", options)
+
+map("n", "<A-9>", ":VimuxRunLastCommand<CR>", options)
+map("n", "<A-0>", ":VimuxPromptCommand<CR>", options)
+
+-- [[ MacOs ]]
+--noremap ƒ :VimuxRunLastCommand<CR>
+--noremap ∂ :VimuxPromptCommand<CR>
