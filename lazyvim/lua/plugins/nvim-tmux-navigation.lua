4dcacd9d4fe385d576d56bce727734b1f09033bc
jad617 <elasmar.jad@gmail.com>
2023-10-01 17:03:11 -0400

    misc

--- lazyvim/lua/plugins/nvim-tmux-navigation.lua
+++ lazyvim/lua/plugins/nvim-tmux-navigation.lua
@@ -2,7 +2,7 @@ return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
     require("nvim-tmux-navigation").setup({
-      disable_when_zoomed = true, -- defaults to false
+      disable_when_zoomed = false, -- defaults to false
       keybindings = {
         left = "<S-Left>",
         down = "<S-Down>",
