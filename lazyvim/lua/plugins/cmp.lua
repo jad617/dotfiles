33fa495042a4ce9b2e8a747cc70558dcbfc4d435
jad617 <15693458+jad617@users.noreply.github.com>
2023-10-03 20:41:24 -0400

    misc

--- lazyvim/lua/plugins/cmp.lua
+++ lazyvim/lua/plugins/cmp.lua
@@ -22,8 +22,6 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.abort(),
