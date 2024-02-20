e1bca5873a498eeaba866496272ae73ee2f348ba
jad617 <elasmar.jad@gmail.com>
2024-02-20 12:04:38 -0500

    misc

--- lazyvim/lua/config/plugins/gitsigns.lua
+++ lazyvim/lua/config/plugins/gitsigns.lua
@@ -26,6 +26,9 @@ require("gitsigns").setup({
 map("n", "<A-f>", ":!git add . && git commit --amend --no-edit && git push -f<CR>", options)
 map("i", "<A-f>", "<C-c>:!git add . && git commit --amend --no-edit && git push -f<CR>", options)
 
+map("n", "<A-/>", ":!gitjpush ", options)
+map("i", "<A-/>", "<C-c>:!gitjpush ", options)
+
 map("n", "<A-a>", ":Gitsigns preview_hunk<CR>", options)
 map("i", "<A-a>", "<C-c>:Gitsigns preview_hunk<CR>", options)
 
