448d5e189cd0a54483e62ff048949a917063292d
jad617 <elasmar.jad@gmail.com>
2024-02-20 14:39:47 -0500

    onedark colors

--- lazyvim/lua/config/plugins/onedark.lua
+++ lazyvim/lua/config/plugins/onedark.lua
@@ -27,6 +27,8 @@ require("onedark").setup({
     ["@number"] = { fg = "#23a0a0" },
     ["@boolean"] = { fg = "#e16d77" },
     ["@type.builtin"] = { fg = "#5fafb9" },
+    ["@constant.builtin"] = { fg = "#ff4d94" },
+    ["@variable.member"] = { fg = "#e16d77" },
   }, -- Override highlight groups
 })
 
