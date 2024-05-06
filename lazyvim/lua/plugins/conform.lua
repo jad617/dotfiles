ecf90c3ab3b725cad0bba904208ba286ba6f9020
jelasmar1 <jad.el-asmar@intelcom.ca>
2024-05-06 12:59:58 -0400

    Add Conform + nvim-lint config

---
+++ lazyvim/lua/plugins/conform.lua
@@ -0,0 +1,33 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      -- All filetypes
+      ["*"] = { "trim_newlines", "trim_whitespace" },
+
+      -- All filetypes that do not have a formatter
+      ["_"] = { "trim_newlines", "trim_whitespace" },
+
+      -- yaml
+      dockerfile = { "trim_newlines", "trim_whitespace" },
+      -- helm = { "trim_newlines", "trim_whitespace" },
+      -- yaml = { "yamlfmt" },
+
+      -- golang
+      go = { "goimports", "gofumpt" },
+
+      -- json
+      json = { "jq" },
+
+      -- markdown
+      markdown = { "markdownlint" },
+
+      -- python
+      python = { "isort", "black" },
+
+      -- terraform
+      hcl = { "terraform_fmt", "tflint" },
+      terraform = { "terraform_fmt", "tflint" },
+    },
+  },
+}
--- lazyvim/lua/plugins/general.lua
+++ lazyvim/lua/plugins/general.lua
@@ -134,7 +134,7 @@ return {
   },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.json" },
 
   -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
