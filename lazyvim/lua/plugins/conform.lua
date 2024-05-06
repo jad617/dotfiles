84e1328d2d1d0e2ba3aa13f9cd7f671af1078ca1
jelasmar1 <jad.el-asmar@intelcom.ca>
2024-05-06 19:14:35 -0400

    fix go formatting

--- lazyvim/lua/plugins/conform.lua
+++ lazyvim/lua/plugins/conform.lua
@@ -3,31 +3,37 @@ return {
   opts = {
     formatters_by_ft = {
       -- All filetypes
-      ["*"] = { "trim_newlines", "trim_whitespace" },
+      -- ["*"] = { "trim_newlines", "trim_whitespace" },
 
       -- All filetypes that do not have a formatter
-      ["_"] = { "trim_newlines", "trim_whitespace" },
+      -- ["_"] = { "trim_newlines", "trim_whitespace" },
 
       -- yaml
       dockerfile = { "trim_newlines", "trim_whitespace" },
-      -- helm = { "trim_newlines", "trim_whitespace" },
-      -- yaml = { "yamlfmt" },
+      helm = { "trim_newlines", "trim_whitespace" },
+      yaml = { "trim_newlines", "trim_whitespace" },
 
-      -- golang
-      go = { "goimports", "gofumpt" },
+      -- golang does not work well with gopls lsp
+      -- go = { "goimports", "gofumpt" },
+      -- go = { "goimports-reviser" },
 
       -- json
-      json = { "jq" },
+      json = { "jq", "trim_newlines", "trim_whitespace" },
 
       -- markdown
-      markdown = { "markdownlint" },
+      markdown = { "markdownlint", "trim_newlines", "trim_whitespace" },
 
       -- python
-      python = { "isort", "black" },
+      python = { "isort", "black", "trim_newlines", "trim_whitespace" },
 
       -- terraform
-      hcl = { "terraform_fmt" },
-      terraform = { "terraform_fmt" },
+      hcl = { "terraform_fmt", "trim_newlines", "trim_whitespace" },
+      terraform = { "terraform_fmt", "trim_newlines", "trim_whitespace" },
+    },
+
+    format = {
+      -- If set to true, causes an issue with LSP gopls
+      lsp_fallback = false,
     },
   },
 }
