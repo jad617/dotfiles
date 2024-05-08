7649a29d1dfc8608ac9c299ca589b63f9a586791
jad617 <15693458+jad617@users.noreply.github.com>
2024-05-08 19:39:24 -0400

    remove html

--- zshNvim/lua/plugins/code/html.lua
+++
@@ -1,9 +0,0 @@
--- html auto reload
-return {
-  "barrett-ruth/live-server.nvim",
-  build = {
-    "sudo npm install -g live-server",
-    "sudo yarn global add live-server",
-  },
-  config = true,
-}
