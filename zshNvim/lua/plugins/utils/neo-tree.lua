e4d34d7b10f1d924bf075322483db73811f48549
jelasmar1 <jad.el-asmar@intelcom.ca>
2024-05-10 13:05:12 -0400

    Add neo-tree expand_all_nodes to open all folders in the view

--- zshNvim/lua/plugins/utils/neo-tree.lua
+++ zshNvim/lua/plugins/utils/neo-tree.lua
@@ -31,6 +31,7 @@ return {
     close_if_last_window = true,
     window = {
       mappings = {
+        ["Z"] = "expand_all_nodes",
         ["<C-v>"] = "open_vsplit",
         ["<C-x>"] = "open_split",
         ["<C-t>"] = "open_tabnew",
