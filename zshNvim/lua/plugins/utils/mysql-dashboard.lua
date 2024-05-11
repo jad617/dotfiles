cc14264d28e8c5175a8bb78521bf9c9879ac4218
jad617 <15693458+jad617@users.noreply.github.com>
2024-05-10 23:17:22 -0400

    cleanup

--- zshNvim/lua/plugins/utils/mysql-dashboard.lua
+++ zshNvim/lua/plugins/utils/mysql-dashboard.lua
@@ -16,9 +16,16 @@ return {
   end,
 
   config = function()
-    vim.cmd(
-      [[autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })]]
-    )
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      pattern = {
+        "sql",
+        "mysql",
+        "plsql",
+      },
+      callback = function()
+        require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+      end,
+    })
   end,
 }
 
