e34c2916768a4a43adf40473a7e13253cc37890b
jad617 <15693458+jad617@users.noreply.github.com>
2024-05-11 10:12:55 -0400

    Added failsafe if file is too big do not load lazy.nvim or any plugins

--- zshNvim/init.lua
+++ zshNvim/init.lua
@@ -1,14 +1,17 @@
 require("config.options")
 
-require("config.lazy")
+local enable_config = require("config.global_functions").FileNotTooBig()
+if enable_config then
+  require("config.lazy")
 
-require("config.autocmds")
-require("config.keymaps")
-require("config.functions")
+  require("config.autocmds")
+  require("config.keymaps")
+  require("config.functions")
 
--- Todo: install
--- lualine,  --> DONE
--- conform, --> DONE
--- nvim-lint, --> DONE
--- fix hcl tfvars
--- telescope root dir if .git available else do current dir
+  -- Todo: install
+  -- lualine,  --> DONE
+  -- conform, --> DONE
+  -- nvim-lint, --> DONE
+  -- fix hcl tfvars
+  -- telescope root dir if .git available else do current dir
+end
