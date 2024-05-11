3f768604609bfd55ee77332d658d9263f0849620
jad617 <15693458+jad617@users.noreply.github.com>
2024-05-11 10:15:21 -0400

    cleanup

--- zshNvim/init.lua
+++ zshNvim/init.lua
@@ -3,15 +3,7 @@ require("config.options")
 local enable_config = require("config.global_functions").FileNotTooBig()
 if enable_config then
   require("config.lazy")
-
   require("config.autocmds")
   require("config.keymaps")
   require("config.functions")
-
-  -- Todo: install
-  -- lualine,  --> DONE
-  -- conform, --> DONE
-  -- nvim-lint, --> DONE
-  -- fix hcl tfvars
-  -- telescope root dir if .git available else do current dir
 end
