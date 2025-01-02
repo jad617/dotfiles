8899792cd15eb427fc8781528c96b4f268f487ab
jad617 <15693458+jad617@users.noreply.github.com>
2025-01-02 00:54:59 -0500

    update neovim snacks terminal config

--- zshNvim/lua/plugins/ui/snacks.lua
+++ zshNvim/lua/plugins/ui/snacks.lua
@@ -130,46 +130,13 @@ return {
       expand = true,
     },
     notifier = { enabled = true },
-    terminal = { win = { style = "float", relative = "editor", border = "double" } },
-    -- quickfile = { enabled = true },
-    -- scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
-    -- words = { enabled = true },
-    -- styles = {
-    --   input = {
-    --     backdrop = false,
-    --     position = "float",
-    --     border = "rounded",
-    --     title_pos = "center",
-    --     height = 1,
-    --     width = 60,
-    --     relative = "editor",
-    --     noautocmd = true,
-    --     row = 2,
-    --     -- relative = "cursor",
-    --     -- row = -3,
-    --     -- col = 0,
-    --     wo = {
-    --       winhighlight = "NormalFloat:SnacksInputNormal,FloatBorder:SnacksInputBorder,FloatTitle:SnacksInputTitle",
-    --       cursorline = false,
-    --     },
-    --     bo = {
-    --       filetype = "snacks_input",
-    --       buftype = "prompt",
-    --     },
-    --     --- buffer local variables
-    --     b = {
-    --       completion = false, -- disable blink completions in input
-    --     },
-    --     keys = {
-    --       n_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "n", expr = true },
-    --       i_esc = { "<esc>", { "cmp_close", "stopinsert" }, mode = "i", expr = true },
-    --       i_cr = { "<cr>", { "cmp_accept", "confirm" }, mode = "i", expr = true },
-    --       i_tab = { "<tab>", { "cmp_select_next", "cmp" }, mode = "i", expr = true },
-    --       q = "cancel",
-    --     },
-    --   },
-    -- },
+    terminal = { win = { style = "float", relative = "editor", border = "double", height = 0.99 } },
+    styles = {
+      zen = {
+        width = 200,
+        relative = "editor",
+      },
+    },
 
     statuscolumn = { enabled = true },
     gitbrowse = { enabled = true },
@@ -177,6 +144,13 @@ return {
   keys = {
     {
       "<leader>z",
+      function()
+        Snacks.zen()
+      end,
+      desc = "Toggle Zoom",
+    },
+    {
+      "<leader>Z",
       function()
         Snacks.zen.zoom()
       end,
