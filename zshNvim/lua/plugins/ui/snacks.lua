22bfbdf5371028a9a83cdcbf8cf134a6925ae0a4
jelasmar1 <jad.el-asmar@intelcom.ca>
2025-02-07 20:10:58 -0500

    misc

--- zshNvim/lua/plugins/ui/snacks.lua
+++ zshNvim/lua/plugins/ui/snacks.lua
@@ -157,17 +157,7 @@ return {
 
     statuscolumn = { enabled = true },
     gitbrowse = { enabled = true },
-    picker = {
-      -- sources = {
-      --   {
-      --     finder = "lsp_definitions",
-      --     format = "file",
-      --     include_current = false,
-      --     auto_confirm = true,
-      --     jump = { tagstack = true, reuse_win = true },
-      --   },
-      -- },
-    },
+    picker = {},
   },
   keys = {
     {
@@ -197,42 +187,6 @@ return {
         Snacks.notifier.show_history()
       end,
       desc = "Notification History",
-    },
-    {
-      "<leader>git",
-      function()
-        Snacks.gitbrowse()
-      end,
-      desc = "Git Browse",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>gb",
-      function()
-        Snacks.git.blame_line()
-      end,
-      desc = "Git Blame Line",
-    },
-    {
-      "<leader>gf",
-      function()
-        Snacks.lazygit.log_file()
-      end,
-      desc = "Lazygit Current File History",
-    },
-    {
-      "<leader>gg",
-      function()
-        Snacks.lazygit()
-      end,
-      desc = "Lazygit",
-    },
-    {
-      "<leader>gl",
-      function()
-        Snacks.lazygit.log()
-      end,
-      desc = "Lazygit Log (cwd)",
     },
     {
       "<leader>un",
@@ -288,6 +242,64 @@ return {
       end,
       desc = "Find Command History",
       mode = { "n" },
+    },
+    -- git
+    {
+      "<leader>git",
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = "Git Browse",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>gb",
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = "Git Branches",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = "Git Log",
+    },
+    {
+      "<leader>gL",
+      function()
+        Snacks.picker.git_log_line()
+      end,
+      desc = "Git Log Line",
+    },
+    {
+      "<leader>gs",
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = "Git Status",
+    },
+    {
+      "<leader>gS",
+      function()
+        Snacks.picker.git_stash()
+      end,
+      desc = "Git Stash",
+    },
+    {
+      "<leader>gd",
+      function()
+        Snacks.picker.git_diff()
+      end,
+      desc = "Git Diff (Hunks)",
+    },
+    {
+      "<leader>gf",
+      function()
+        Snacks.picker.git_log_file()
+      end,
+      desc = "Git Log File",
     },
 
     -- LSP
