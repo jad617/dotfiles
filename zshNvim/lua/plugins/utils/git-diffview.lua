d8d09834bb59b7e09cc9a1afe9b45ccf55b6154c
jad617 <15693458+jad617@users.noreply.github.com>
2025-02-09 02:54:52 -0500

    Add DiffViewOpen key bindings

--- zshNvim/lua/plugins/utils/git-diffview.lua
+++ zshNvim/lua/plugins/utils/git-diffview.lua
@@ -1,1 +1,15 @@
-return { "sindrets/diffview.nvim" }
+return {
+  "sindrets/diffview.nvim",
+  config = function()
+    ------------------------------------------------------------
+    -- [[ local vars ]]
+    ------------------------------------------------------------
+    local map = vim.api.nvim_set_keymap -- set keys
+    local options_silent = { noremap = true, silent = true }
+
+    ------------------------------------------------------------
+    -- [[ Key Bindings ]]
+    ------------------------------------------------------------
+    map("n", "<leader>gm", "<cmd>DiffviewOpen<CR>", options_silent)
+  end,
+}
