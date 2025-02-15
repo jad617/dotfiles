257e30d976ebfaa26ad5fc4e69000ce762fe57db
jad617 <15693458+jad617@users.noreply.github.com>
2025-02-14 22:49:01 -0500

    update onedark

--- zshNvim/lua/config/colorscheme.lua
+++ zshNvim/lua/config/colorscheme.lua
@@ -3,3 +3,7 @@
 -- vim.cmd.colorscheme("nordic")
 -- vim.cmd.colorscheme("tokyonight-storm")
 vim.cmd.colorscheme("onedark")
+
+-- Change background color
+-- vim.cmd("highlight Normal guibg=#1F262F")
+-- vim.cmd("highlight EndOfBuffer guibg=#1F262F")
