4d3a2495ffd7cba1b3fcf098edb5ca9dd0a90bb2
jelasmar1 <jad.el-asmar@intelcom.ca>
2025-05-23 09:39:47 -0400

    Add markdown A+m commande

--- zshNvim/lua/plugins/ui/markdown.lua
+++ zshNvim/lua/plugins/ui/markdown.lua
@@ -12,5 +12,17 @@ return {
       -- You can choose one of the following pickers
       "nvim-telescope/telescope.nvim",
     },
+    config = function()
+      ------------------------------------------------------------
+      -- [[ local vars ]]
+      ------------------------------------------------------------
+      local map = vim.api.nvim_set_keymap -- set keys
+      local options = { noremap = true, silent = true }
+
+      ------------------------------------------------------------
+      -- [[ Key Bindings ]]
+      ------------------------------------------------------------
+      map("n", "<A-m>", ":LivePreview start<CR>", options)
+    end,
   },
 }
