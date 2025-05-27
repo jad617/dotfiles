7d63cb812ecc111961f04e793c0e5c6a52ca73ae
jad617 <15693458+jad617@users.noreply.github.com>
2025-05-26 21:54:11 -0400

    cleanup

--- zshNvim/lua/config/functions.lua
+++ zshNvim/lua/config/functions.lua
@@ -33,45 +33,6 @@ end
 vim.api.nvim_set_keymap("n", "<leader>8", "<cmd>lua Search_current_word()<CR>", options)
 
 ------------------------------------------------------------
--- [[ Open Notes ]]
-------------------------------------------------------------
--- function OpenNotes(filename)
---   if not (filename == "") then
---     local root_dir = "/home/jelasmar/notes"
---     local dir = root_dir
---
---     -- Split the filename into directory and file components
---     local path_separator = "/"
---     local subdirs = {}
---     local file = filename
---
---     -- If filename contains '/'
---     if filename:find(path_separator) then
---       subdirs = vim.split(filename, path_separator, true) -- Split the filename by '/'
---       file = subdirs[#subdirs] -- Get the last component as the filename
---       table.remove(subdirs) -- Remove the last component from the list (the filename itself)
---     end
---
---     -- Create subdirectories if they don't exist
---     for _, subdir in ipairs(subdirs) do
---       dir = dir .. "/" .. subdir:gsub("%s+", "")
---       if vim.fn.isdirectory(dir) == 0 then
---         vim.fn.mkdir(dir, "p")
---       end
---     end
---
---     -- Open the file in the appropriate directory
---     vim.cmd("tabnew " .. dir .. "/" .. file)
---
---     -- Show directory tree
---     vim.defer_fn(OpenNeotree, 100)
---   end
--- end
---
--- map("i", "<A-n>", '<C-c>:lua OpenNotes(vim.fn.input("Note file to open:"))<CR> ', options)
--- map("n", "<A-n>", ':lua OpenNotes(vim.fn.input("Note file to open:"))<CR> ', options)
-
-------------------------------------------------------------
 -- [[ OpenNotesTelescope ]]
 ------------------------------------------------------------
 -- Allows to open Telescope in our notes directory to open or create new notes
@@ -84,7 +45,7 @@ function OpenNotesTelescope()
   end
 
   vim.api.nvim_set_current_dir(full_path)
-  vim.cmd(":Telescope file_browser")
+  vim.cmd(":lua Snacks.picker.files({hidden = true})")
 end
 
 map("i", "<A-n>", "<C-c>:lua OpenNotesTelescope()<CR>", options)
