8400440b7c8dcbeaf20494a1f77dc4ebcc39f46a
jad617 <15693458+jad617@users.noreply.github.com>
2025-05-28 22:51:25 -0400

    new keybindings

--- zshNvim/init.lua
+++ zshNvim/init.lua
@@ -1,10 +1,10 @@
 require("config.options")
 
-local enable_config = require("config.global_functions").FileNotTooBig()
-if enable_config then
-  require("config.lazy")
-  require("config.autocmds")
-  require("config.keymaps")
-  require("config.functions")
-  require("config.colorscheme")
-end
+-- local enable_config = require("config.global_functions").FileNotTooBig()
+-- if enable_config then
+require("config.lazy")
+require("config.autocmds")
+require("config.keymaps")
+require("config.functions")
+require("config.colorscheme")
+-- end
